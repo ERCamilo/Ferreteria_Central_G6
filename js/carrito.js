dcdfde7d79e85f0aa9b51c934102f251a259287d
santaalejandragonzalezlagual-dot <santaalejandragonzalezlagual@gmail.com>
2026-03-17 22:57:08 -0400

    Add shopping cart functionality in carrito.js
    Implement functions to display, remove, and clear items in the shopping cart.

--- js/carrito.js
+++ js/carrito.js
@@ -1,1 +1,66 @@
+let carrito = JSON.parse(localStorage.getItem("carrito")) || [];
 
+const contenedor = document.getElementById("carrito-container");
+const totalElemento = document.getElementById("total");
+
+function mostrarCarrito(){
+
+contenedor.innerHTML = "";
+
+let total = 0;
+
+carrito.forEach((producto, index) => {
+
+let precio = parseFloat(producto.precio.replace("$",""));
+
+let subtotal = precio;
+
+total += subtotal;
+
+contenedor.innerHTML += `
+
+<div class="cart-item">
+
+<h3>${producto.nombre}</h3>
+
+<p>Precio: ${producto.precio}</p>
+
+<p>Subtotal: $${subtotal.toFixed(2)}</p>
+
+<button onclick="eliminarProducto(${index})">
+Eliminar
+</button>
+
+</div>
+
+<hr>
+
+`;
+
+});
+
+totalElemento.textContent = total.toFixed(2);
+
+}
+
+function eliminarProducto(index){
+
+carrito.splice(index,1);
+
+localStorage.setItem("carrito", JSON.stringify(carrito));
+
+mostrarCarrito();
+
+}
+
+function vaciarCarrito(){
+
+localStorage.removeItem("carrito");
+
+carrito = [];
+
+mostrarCarrito();
+
+}
+
+mostrarCarrito();
